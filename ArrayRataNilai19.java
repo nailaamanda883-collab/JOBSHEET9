766d11ffa2fd59f7fa82ee50063fa28443d00bf9
nailaamanda883 <nailaamanda883@gmail.com>
2025-11-06 14:51:30 +0700

    modif2

--- ArrayRataNilai19.java
+++ ArrayRataNilai19.java
@@ -1,29 +1,33 @@
 import java.util.Scanner;
 
-public class ArrayRataNilai19 {
+public class ArrayRataNilai19 { 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int[] nilaiMhs= new int[10];
-        double total = 0;
-        double rata2;
-        int lulus = 0;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Masukkan Jumlah Mahasiswa: ");
+         int jumlah = input.nextInt();
+        int[] nilaiMhs = new int[jumlah];
+        double totalLulus = 0, totalTidakLulus = 0;
+        int jumlahLulus = 0, jumlahTidakLulus = 0;
 
-        for (int i=0; i < nilaiMhs.length; i++) {
-            System.out.print("Masukkan nilai mahasiswa ke-" +(i+1)+ " : ");
-            nilaiMhs[i] = sc.nextInt();
-        }
-        for (int i=0; i< nilaiMhs.length; i++) {
-            total += nilaiMhs[i];
-        }
-        rata2 = total/nilaiMhs.length;
-        System.out.println("Rata-rata nilai = " + rata2);
-        
-        for (int i = 0; i < nilaiMhs.length; i++) {
+        for (int i = 0; i < jumlah; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + " : ");
+            nilaiMhs[i] = input.nextInt();
+
             if (nilaiMhs[i] > 70) {
-            lulus++;
+                totalLulus += nilaiMhs[i];
+                jumlahLulus++;
+            } else {
+                totalTidakLulus += nilaiMhs[i];
+                jumlahTidakLulus++;
             }
         }
+        double rataLulus = (jumlahLulus > 0) ? (totalLulus / jumlahLulus) : 0;
+        double rataTidakLulus = (jumlahTidakLulus > 0) ? (totalTidakLulus / jumlahTidakLulus) : 0;
 
-        System.out.println("Jumlah mahasiswa yang lulus: " + lulus);
+        System.out.println("Rata-rata nilai lulus = " + rataLulus);
+        System.out.println("Rata-rata nilai tidak lulus = " + rataTidakLulus);
+
+
+
     }
 }
